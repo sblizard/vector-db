63a9471e1f8e3ec502acf1a93b5b5b994b6a1d5d
Sean Blizard <emailseanblizard@gmail.com>
2025-10-26 13:41:00 -0400

    [feat] add /hello and /headers routes

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,9 +3,22 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/sblizard/vector-db/internal/config"
 )
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(w, "Hello, World!")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
 
 func main() {
 	cfg, err := config.Load("config.json")
@@ -15,4 +28,9 @@ func main() {
 
 	fmt.Printf("Server starting on port %d...\n", cfg.Server.Port)
 	fmt.Printf("Using %d subspaces, vector dim: %d\n", cfg.NumSubspaces, cfg.Dim)
+
+	http.HandleFunc("/", hello)
+	http.HandleFunc("/headers", headers)
+
+	http.ListenAndServe(fmt.Sprintf(":%d", cfg.Server.Port), nil)
 }
